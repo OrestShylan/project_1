d8c839e9a1712f3e5973a04f863156444aefe3d4
OrestShylan <orestshylan1997@gmail.com>
2023-12-17 17:19:16 +0200

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from 'Layout/Footer/Footer';
 import Header from 'Layout/Header/Header';
 import Home from 'pages/Home/Home';
+import './scss/style.scss'
 
 export const App = () => {
   return (
